c699ed93568900ebde86908c0b12246b61a24246
AasmaQaisar <125805604+AasmaQaisar@users.noreply.github.com>
2023-04-07 04:33:06 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,21 @@
+
+ // JSON object
+//Example 1
+ const detail = { "name": "Aasma", "age": 25 , "city:": "Lahore"}
+
+ // accessing JSON object
+ console.log(detail. name); 
+ console.log((detail));
+ 
+
+ //JSON object
+ //Example 2
+
+ const person = {
+    firstName: "Jahanzaib",
+    lastName: "Tayyab",
+    age: 24,
+    city: "Lahore",
+  };
+  // accessing JSON object
+  console.log(person);
